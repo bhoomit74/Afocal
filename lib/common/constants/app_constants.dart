5e1e2619ddde52044b65a5f5c33c981dea7c6464
bhoomit <bhfirebase74@gmail.com>
2024-04-05 03:44:26 +0530

    Style: Add style constants

---
+++ lib/common/constants/app_constants.dart
@@ -0,0 +1,6 @@
+class AppConstants {
+  static const double defaultPadding = 40;
+  static const double mediumPadding = 20;
+  static const double smallPadding = 10;
+  static const double buttonBorderRadius = 32;
+}
